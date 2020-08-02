b42674effe92623815a57cb9ea9e665301f37ff4
ラムセスソラノラミレズ <ramsessr@icloud.com>
2020-08-02 15:06:49 -0500

    add delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, NgForm,  Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +6,5 @@ import { FormControl, NgForm,  Validators, FormGroup, FormBuilder } from '@angul
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'example-one-with-component';
-  form: FormGroup;
-  taskFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      task: this.taskFormControl
-    });
-  }
-
-  tasks: Array<string> = [];
-
-  addToTasks = (): void => {
-    console.log( this.taskFormControl.value );
-    this.tasks.push(this.taskFormControl.value);
-  }
+  
 }
--- src/app/components/list-tasks/list-tasks.component.ts
+++ src/app/components/list-tasks/list-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list-tasks',
@@ -7,11 +8,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListTasksComponent implements OnInit {
 
-  @Input() tasks: Array<string>;
+  emailFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  tasks: Array<string> = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addToTasks = (): void => {
+    this.tasks.push(this.emailFormControl.value);
+  }
+
+  deleteElement = (index: number): void => {
+    this.tasks.splice(index, 1);
+  }
 }
